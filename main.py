4d991cb7488d6eac9db1573db0590b45230d6939
tinker <1539214893@qq.com>
2025-04-20 16:42:31 +0800

    fix: 新增调试信息

--- main.py
+++ main.py
@@ -470,24 +470,29 @@ class MyPlugin(Star):
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+        logger.info("插件初始化完成！")
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
-
+        logger.info("插件销毁完成！")
 
     @filter.command("jrys", alias={'今日运势', '运势'})
     async def jrys(self, event: AstrMessageEvent) -> MessageEventResult:
         """ 今日运势 """
+        logger.info("收到今日运势请求")
         try:
             yield event.chain_result("正在分析你的运势哦~请稍等~~")
             # 获取用户ID 和 头像
             user_id = event.get_sender_id()
             avatar = f"https://q4.qlogo.cn/headimg_dl?dst_uin={user_id}&spec=640"
+            logger.info(f"用户ID: {user_id}, 头像URL: {avatar}")
             # 生成今日运势图片
             html = generate_fortune_html(user_id, avatar)
             # 发送图片
+            logger.info("生成今日运势图片")
             url = await self.html_render(html, {})
             yield event.image_result(url)
+            logger.info("今日运势图片发送成功")
         except Exception as e:
             logger.error(f"Error generating fortune image: {e}")
             yield event.chain_result("生成今日运势图片失败，请稍后再试。")
